a7c0c7913621136583e2684ec166b4bc0394ef03
Lasserini <lasserini@gmail.com>
2021-09-10 10:47:10 +0000

    Add incorrectGuess function

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,16 +38,68 @@ let hidden_words = [
     "ELECTRICITY"
 ]
 
+// Variables used accoss multiple functions
+let secretWord = [];
+
 // Function to select the secret word to guess
 function selectSecretWord(){
-    let secretWord = hidden_words[Math.floor(Math.random() * hidden_words.length)];
+    secretWord = hidden_words[Math.floor(Math.random() * hidden_words.length)];
     secretWord = secretWord.split("").map(letter => " _ ").join("");
     document.getElementById("secret_word").innerHTML = secretWord;
 }
 
-// Function to check if guess is correct
-function checkGuess(letter){
+// Functions for the conclusions
+function lostGame(){
+    document.getElementById("globe").src ="assets/images/failure_image.jpg";
+}
 
+function wonGame(){
+
+}
+
+// Functions to handle corret & incorrest guesses
+function correctGuess(){
+
+}
+
+function incorrectGuess(){
+    let remainingGuesses = document.getElementById("remaining_guesses");
+    switch (remainingGuesses) {
+        case "6":
+            document.getElementById("globe").src ="assets/images/stage2.jpg";
+            remainingGuesses --;
+            document.getElementById("remaining_guesses").innerHTML = remainingGuesses;
+            break;
+        case "5":
+            document.getElementById("globe").src ="assets/images/stage3.jpg";
+            remainingGuesses --;
+            document.getElementById("remaining_guesses").innerHTML = remainingGuesses;
+            break;
+        case "4":
+            document.getElementById("globe").src ="assets/images/stage4.jpg";
+            remainingGuesses --;
+            document.getElementById("remaining_guesses").innerHTML = remainingGuesses;
+            break;
+        case "3":
+            document.getElementById("globe").src ="assets/images/stage5.jpg";
+            remainingGuesses --;
+            document.getElementById("remaining_guesses").innerHTML = remainingGuesses;
+            break;
+        case "2":
+            document.getElementById("globe").src ="assets/images/stage6.jpg";
+            remainingGuesses --;
+            document.getElementById("remaining_guesses").innerHTML = remainingGuesses;
+            break;
+        default:
+            lostGame();
+            break;
+    }
+}
+
+function checkGuess(letter){
+    if (secretWord.indexOf(letter) >= 0) {
+
+    }
 
     document.getElementById(letter).disabled = true;
 }
@@ -62,7 +114,7 @@ function startGame(){
         addGuessCounter.style.display = "block";
     let addGameArea = document.getElementById("game_area");
         addGameArea.style.display = "block";
-    selectSecretWord()
+    selectSecretWord();
 }
 
 // Functions that resets the GameState page when Play Again button is pressed.
@@ -72,7 +124,7 @@ function reStartGame(){
     document.getElementById("globe").src ="assets/images/stage1.jpg";
     document.getElementById("remaining_guesses").innerHTML = 6;
     document.getElementById("A").disabled = false;
-    selectSecretWord()
+    selectSecretWord();
 }
 
 
